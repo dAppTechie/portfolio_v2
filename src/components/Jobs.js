9ba1635116ba6010982ea89d13338f3541aec961
Chad Parker <63312621+imchad-me@users.noreply.github.com>
2021-07-15 16:59:27 -0600

    Experience section complete

--- src/components/Jobs.js
+++ src/components/Jobs.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import Title from "./Title"
 import { FaOldRepublic, FaMandalorian } from "react-icons/fa"
 import { graphql, useStaticQuery } from "gatsby"
@@ -6,9 +6,10 @@ import { Link } from "gatsby"
 
 const query = graphql`
   {
-    allStrapiJob {
+    allStrapiJob(sort: { fields: created_at, order: DESC }) {
       nodes {
         position
+        short
         company
         date
         desc {
@@ -25,12 +26,25 @@ const Jobs = () => {
   const {
     allStrapiJob: { nodes: jobs },
   } = data
-  const { company, position, date, desc } = jobs[0]
+  const [value, setValue] = useState(0)
+  const { company, position, date, desc } = jobs[value]
   return (
     <section className="section jobs">
       <Title title="experience" />
       <div className="jobs-center">
-        <div className="btn-container"></div>
+        <div className="btn-container">
+          {jobs.map((item, index) => {
+            return (
+              <button
+                key={index}
+                className={index === value ? "job-btn active-btn" : "job-btn"}
+                onClick={() => setValue(index)}
+              >
+                {item.short}
+              </button>
+            )
+          })}
+        </div>
         <article className="job-info">
           <h3>{position}</h3>
           <h4>{company}</h4>

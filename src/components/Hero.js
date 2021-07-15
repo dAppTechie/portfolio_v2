131b582c611f0f4b10c9bf30acef56a4a4bec88b
Chad Parker <63312621+imchad-me@users.noreply.github.com>
2021-07-14 19:57:44 -0600

    new logo, favicon, navbar styling and hero styling

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,11 +1,48 @@
 import React from "react"
 import { Link } from "gatsby"
 import socialLinks from "../constants/social_links"
-// import heroImg from "../assets/images/hero.svg"
-import { StaticImage } from "gatsby-plugin-image"
 
 const Hero = () => {
-  return <h2>hero component</h2>
+  return (
+    <header className="hero">
+      <section className="section-center hero-center">
+        <article className="hero-info">
+          <div>
+            <div className="underline"></div>
+            <div className="content">
+              <h1>I'm Chad,</h1>
+              <p>
+                {" "}
+                a <span className="bold">Full-Stack Developer</span> that's
+                committed to <span className="bold">Humanitarian</span>,{" "}
+                <span className="bold">Social</span> and{" "}
+                <span className="bold">Environmental Causes</span>.
+              </p>
+              <p>
+                {" "}
+                My goal is to use <span className="bold">code</span> to bring
+                together <span className="bold">youth</span> from{" "}
+                <span className="bold">different cultures</span>, and{" "}
+                <span className="bold">teach them valuable life skills</span>.
+              </p>
+            </div>
+            <Link to="/contact" className="btn">
+              contact me
+            </Link>
+            <div className="social-links">
+              {socialLinks.map(link => {
+                return (
+                  <a href={link.url} key={link.id} className="social-link">
+                    {link.icon}
+                  </a>
+                )
+              })}
+            </div>
+          </div>
+        </article>
+      </section>
+    </header>
+  )
 }
 
 export default Hero
--- src/constants/social_links.js
+++ src/constants/social_links.js
@@ -1,6 +1,6 @@
 import React from "react"
 import {
-  FaFacebookSquare,
+  FaGithubSquare,
   FaLinkedin,
   FaDribbbleSquare,
   FaBehanceSquare,
@@ -10,28 +10,18 @@ import {
 const data = [
   {
     id: 1,
-    icon: <FaFacebookSquare className="social-icon"></FaFacebookSquare>,
-    url: "https://www.twitter.com",
+    icon: <FaGithubSquare className="social-icon"></FaGithubSquare>,
+    url: "https://github.com/imchad-me",
   },
   {
     id: 2,
     icon: <FaLinkedin className="social-icon"></FaLinkedin>,
-    url: "https://www.twitter.com",
+    url: "https://www.linkedin.com/in/parkerchadlee/",
   },
   {
     id: 3,
-    icon: <FaDribbbleSquare className="social-icon"></FaDribbbleSquare>,
-    url: "https://www.twitter.com",
-  },
-  {
-    id: 4,
-    icon: <FaBehanceSquare className="social-icon"></FaBehanceSquare>,
-    url: "https://www.twitter.com",
-  },
-  {
-    id: 5,
     icon: <FaTwitterSquare className="social-icon"></FaTwitterSquare>,
-    url: "https://www.twitter.com",
+    url: "https://twitter.com/imchad_me",
   },
 ]
 

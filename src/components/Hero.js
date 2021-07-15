a290d15c12677b82eecf44c19cf4d736e5747b9b
Chad Parker <63312621+imchad-me@users.noreply.github.com>
2021-07-14 21:37:01 -0600

    Services section complete

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,6 +1,8 @@
 import React from "react"
 import { Link } from "gatsby"
 import socialLinks from "../constants/social_links"
+// import heroImg from "../assets/images/hero.svg"
+import { StaticImage } from "gatsby-plugin-image"
 
 const Hero = () => {
   return (
@@ -40,6 +42,12 @@ const Hero = () => {
             </div>
           </div>
         </article>
+        <StaticImage
+          src="../assets/images/hero.svg"
+          alt="portfolio"
+          className="hero-img"
+          placeholder="blurred"
+        />
       </section>
     </header>
   )
--- src/constants/services.js
+++ src/constants/services.js
@@ -1,23 +1,23 @@
 import React from "react"
-import { FaCode, FaSketch, FaAndroid } from "react-icons/fa"
+import { FaCode, FaNodeJs, FaMobileAlt } from "react-icons/fa"
 const services = [
   {
     id: 1,
     icon: <FaCode className="service-icon" />,
     title: "web development",
-    text: `I'm baby meditation tofu chillwave, distillery messenger bag thundercats chicharrones kale chips gochujang. Banh mi direct trade marfa salvia.`,
+    text: `My layouts can be dynamic or static. All layouts are responsive and work on any device.`,
   },
   {
     id: 2,
-    icon: <FaSketch className="service-icon" />,
-    title: "web design",
-    text: `I'm baby meditation tofu chillwave, distillery messenger bag thundercats chicharrones kale chips gochujang. Banh mi direct trade marfa salvia.`,
+    icon: <FaNodeJs className="service-icon" />,
+    title: "backend development",
+    text: `Able to develop the backend with knowledge of multiple backend languages such as Node, Python (Django), and Java (Spring).`,
   },
   {
     id: 3,
-    icon: <FaAndroid className="service-icon" />,
-    title: "app design",
-    text: `I'm baby meditation tofu chillwave, distillery messenger bag thundercats chicharrones kale chips gochujang. Banh mi direct trade marfa salvia.`,
+    icon: <FaMobileAlt className="service-icon" />,
+    title: "app development",
+    text: `Visually pleasing apps that are optimized to swiftly grow. Strong preference for easy to use, intuitive UX/UI.`,
   },
 ]
 

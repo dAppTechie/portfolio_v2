ee0e2163822cad4760185ec9a046cfdd48d56781
Chad Parker <63312621+imchad-me@users.noreply.github.com>
2021-07-15 22:28:45 -0600

    contact form complete

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,7 +3,43 @@ import Seo from "../components/Seo"
 const contact = () => {
   return (
     <>
-      <h2>contact page</h2>
+      <section className="contact-page">
+        <article className="contact-form">
+          <h3>get in touch</h3>
+          <form
+            method="post"
+            netlify-honeypot="bot-field"
+            data-netlify="true"
+            name="contact"
+          >
+            <input type="hidden" name="bot-field" />
+            <input type="hidden" name="form-name" value="contact" />
+            <div className="form-group">
+              <input
+                type="text"
+                name="name"
+                placeholder="name"
+                className="form-control"
+              />
+              <input
+                type="email"
+                name="email"
+                placeholder="email"
+                className="form-control"
+              />
+              <textarea
+                name="message"
+                rows="5"
+                placeholder="message"
+                className="form-control"
+              ></textarea>
+            </div>
+            <button type="submit" className="submit-btn btn">
+              Submit
+            </button>
+          </form>
+        </article>
+      </section>
     </>
   )
 }

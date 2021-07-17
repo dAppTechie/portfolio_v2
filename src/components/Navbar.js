64be37699a76d04d848081eed4bfa6f46ce8fd9c
Chad Parker <63312621+imchad-me@users.noreply.github.com>
2021-07-17 13:58:56 -0600

    update logo for small screens

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,17 @@
 import React from "react"
 import logo from "../assets/images/logo.svg"
+import logoSmall from "../assets/images/logomin.svg"
 import { FaAlignRight } from "react-icons/fa"
 import pageLinks from "../constants/links"
 import { Link } from "gatsby"
+
 const Navbar = ({ toggleSidebar }) => {
   return (
     <nav className="navbar">
       <div className="nav-center">
         <div className="nav-header">
-          <img src={logo} alt="Cp" />
+          <img src={logo} alt="ChadParker" className="logo-main" />
+          <img src={logoSmall} alt="Cp" className="logo-small" />
           <button className="toggle-btn" type="button" onClick={toggleSidebar}>
             <FaAlignRight />
           </button>
